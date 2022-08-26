232005c65386d41fe51b1c3f8e3bf24eb6e43aff
allaskme <13313029860lic@gmail.com>
2022-08-26 15:05:59 +0800

    添加项目文件。

---
+++ CSharpS01E02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpS01E02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
