ea62b2f8c6a1100336ca232013686ca7f987b696
allaskme <13313029860lic@gmail.com>
2022-09-20 17:26:14 +0800

    新增变量内容

---
+++ CSharpS01E03Variable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpS01E03Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建变量，就是创建一个存储数据的容器
+            int age;    //声明了一个名称为age的整形变量
+
+            age = 10;   //变更的赋值
+
+            // 常用的基本变量类型有：byte short int long float double char bool
+            // 变量名区分大小写，可以是字母、数字、下划线的组合，但数字不能开头
+        }
+    }
+}
