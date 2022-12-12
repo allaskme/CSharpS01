685839227c26130a2ac7aa20fe33b91b4b7be5ee
allaskme <13313029860lic@gmail.com>
2022-12-12 14:34:51 +0800

    继续学习

--- CSharpS01E05CharacterPractice/Program.cs
+++ CSharpS01E05CharacterPractice/Program.cs
@@ -9,6 +9,18 @@ namespace CSharpS01E05CharacterPractice
             // 练习1：得到用户输入的两个数字，然后将两个数字相加，并将和输出到控制台
             int a;
             int b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b);
+
+            if (a < b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+                Console.WriteLine(b);
+
+
         }
     }
 }
---
+++ CSharpS01E15SortNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharpS01E15SortNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                int temp = a; a = b; b = temp;
+            }
+            if (b > c)
+            {
+                int temp = b; b = c; c = temp;
+            }
+            if (a > b)
+            {
+                int temp = a; a = b; b = temp;
+            }
+            Console.WriteLine(a + " " + b + " " + c);
+        }
+    }
+}
