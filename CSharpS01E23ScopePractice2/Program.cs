5f2ef9effaa2dc83b6e9ec4c08aeffb831b89e45
allaskme <13313029860lic@gmail.com>
2023-04-18 17:40:21 +0800

    继续学习

--- CSharpS01E23ScopePractice2/Program.cs
+++ CSharpS01E23ScopePractice2/Program.cs
@@ -4,15 +4,46 @@
     {
         static void Main(string[] args)
         {
+            // 一个小球从height高度下落，经过10次落地和反弹，每次反弹是前次反弹高度的一半，求小球10次落地过程中的总行程及10次落地每次的高度
             double height = Convert.ToDouble(Console.ReadLine());
-            double sum = 0;
+            double sum = height;
 
-            for(int i = 0; i < 10; i++)
+            Console.WriteLine($"初始的高度是：{height}");
+
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"");
-                sum += height / 2;
+                Console.WriteLine($"第{i + 1}次落地的行程是：{sum}");
+                height /= 2;
+                Console.WriteLine($"第{i + 1}次反弹的高度是：{height}");
+                if (i != 9)
+                {
+                    sum += height * 2;
+                }
             }
-            sum += 10;
+            Console.WriteLine($"小球总的行程是{sum}");
+            //sum += 10;
+
+            // 类的实例变量、静态变量以及常量
+            Test test = new Test();
+            test.Mem1 = 1;
+            Test.Count();
+            Console.WriteLine(test.Mem1 + " 计数："+ Test.Mem2);
+            Test test1 = new Test();
+            test1.Mem1 = 2;
+            Test.Count();
+            Console.WriteLine(test.Mem1 + "   " + test1.Mem1 + " 计数：" + Test.Mem2);
+
+        }
+
+        class Test
+        {
+            public int Mem1;
+            static public int Mem2 = 0;
+            
+            static public void Count()
+            {
+                Mem2++;
+            }
         }
     }
 }
---
+++ CSharpS01E24ScopePractice3/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharpS01E24ScopePractice3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 输入一个整数a，和一个正整数n，计算a的n次方
+            int a = Convert.ToInt32 (Console.ReadLine());
+            int n = Convert.ToInt32 (Console.ReadLine());
+            int loop = n;
+            int sum = 1;
+
+            while(loop > 0)
+            {
+                sum *= a;
+                loop--;
+            }
+            Console.WriteLine ($"{a}的{n}次方是{sum}");
+
+            // 输入一个正整数n，求n的阶乘
+
+            n = Convert.ToInt32 (Console.ReadLine());
+
+            int factorial = Factorial(n);
+            Console.WriteLine($"{n}的阶乘是{factorial}");
+        }
+
+        // 递归函数
+        static public int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
