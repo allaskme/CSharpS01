6b691bb75b753e9ab6d17941ab6725ddd8f98d7b
allaskme <13313029860lic@gmail.com>
2023-08-24 17:32:00 +0800

    继续学习

--- CSharpS01E29/Program.cs
+++ CSharpS01E29/Program.cs
@@ -17,6 +17,53 @@ namespace CSharpS01E29
              * 输出4行，每行10个*
              */
             PrintStar2();
+
+            /*
+             * 编程题3
+             * 输入两个整数m和n，输出m行n列的*阵
+             */
+            PrintStar3();
+
+
+            /*
+             * 编程题4
+             * 输入整数n，输出n行，每行n个*的星形三角形
+             * 样例：
+             * 输入：3
+             * 输出：
+             * *
+             * **
+             * ***
+             */
+            PrintStar4();
+
+            /*
+             * 编程题5
+             * 输入整数k，输出k行三角形的斜边
+             * 样例：
+             * 输入：5
+             * 输出：
+             * *
+             *  *
+             *   *
+             *    *
+             *     *
+             */
+            PrintStar5();
+
+            /*
+             * 编程题6
+             * 输入整数x，输出x行三角形的斜边，但输出与题5相反
+             * 样例：
+             * 输入：5
+             * 输出：
+             *     *
+             *    *
+             *   *
+             *  *
+             * *
+             */
+            PrintStar6();
         }
 
         static void PrintStar1()
@@ -29,6 +76,7 @@ namespace CSharpS01E29
             Console.WriteLine();
             Console.WriteLine();
         }
+
         static void PrintStar2()
         {
             Console.WriteLine("-------编程题2-------");
@@ -40,6 +88,84 @@ namespace CSharpS01E29
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void PrintStar3()
+        {
+            Console.WriteLine("-------编程题3-------");
+            Console.Write("请输入行数m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("请输入列数n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void PrintStar4()
+        {
+            Console.WriteLine("-------编程题4-------");
+            Console.Write("请输入行列数n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void PrintStar5()
+        {
+            Console.WriteLine("-------编程题5-------");
+            Console.Write("请输入行列数k:");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= k; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void PrintStar6()
+        {
+            Console.WriteLine("-------编程题6-------");
+            Console.Write("请输入行列数x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = x; i >= 1; i--)
+            {
+                for (int j = i - 1; j >= 1; j--)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
---
+++ CSharpS01E30/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace CSharpS01E30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 编程题1
+             * 输入正整数a，输出a行，每行a个*的平行四边形
+             * 样例：
+             * 输入：4
+             * 输出：
+             * ****
+             *  ****
+             *   ****
+             *    ****
+             */
+            PrintStar1();
+
+            /*
+             * 编程题2
+             * 输入一个正整数b，输出b层的右三角形
+             * 样例：
+             * 输入：5
+             * 输出：
+             *     *
+             *    **
+             *   ***
+             *  ****
+             * ***** 
+             */
+            PrintStar2();
+
+            /*
+             * 编程题3
+             * 输入一个正整数c，输出c层的等腰三角形
+             * 样例：4
+             * 输出：
+             *    *
+             *   ***
+             *  *****
+             * ******* 
+             */
+            PrintStar3();
+        }
+
+        static void PrintStar1()
+        {
+            Console.WriteLine("----------编程题1-----------");
+            Console.Write("请输入正整数a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < a; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void PrintStar2()
+        {
+            Console.WriteLine("----------编程题2-----------");
+            Console.Write("输入一个正整数b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= b; i++)
+            {
+                // n-i个空格，i个*
+                for (int j = b - i; j > 0; j--)
+                // for (int j = 0; j < n-i;j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void PrintStar3()
+        {
+            Console.WriteLine("----------编程题3-----------");
+            Console.Write("输入一个正整数c:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            //两种方法之一：
+            //先用编程题2的方法按行输出左边三角形的每一行，然后在循环中每行追加右边的三角形，但需要每行减1操作
+            for (int i = 1; i <= c; i++)
+            {
+                for (int j = 0; j < c - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            //两种方法之二：
+            //先输出每行的空格，空格数是c - i个，然后输出*，每行输出的*的个数是2*i - 1个
+            for (int i = 1; i <= c; i++)
+            {
+                for (int j = 0; j < c - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
